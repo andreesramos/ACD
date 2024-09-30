03ae8965a82b8b07fc143ae37418203dacd8bfed
andreesramos <andresrn16@gmail.com>
2024-09-30 12:44:37 +0200

    a

--- Tema1/Actividad2.py
+++ Tema1/Actividad2.py
@@ -1,6 +1,21 @@
-import math
+from pathlib import Path
 
-numero = input("Introduce un número por favor: ");
-numero_float = float(numero);
-raiz_cuadrada = math.sqrt(numero_float);
-print("La raíz cuadrada es: " , raiz_cuadrada);
+class FileManager:
+    def write_file(self, file_path, content, mode='w'):
+        try:
+            with open(file_path, mode) as archivo:
+                archivo.write(content)
+        except Exception as e:
+            print("Error escribiendo el achivo: {e}")
+
+    def read_file(self, file_path, mode='r'):
+        try:
+            with open(file_path, mode) as archivo:
+                content=archivo.read()
+                print(content)
+        except Exception as e:
+            print("Error leyendo el archivo: {e}")
+
+file_manager=FileManager()
+file_manager.write_file("24525402.txt", "16/03/2003")
+file_manager.read_file("24525402.txt")
--- Tema1/Actividad3.py
+++ Tema1/Actividad3.py
@@ -1,5 +1,23 @@
-import math
+import json
 
-num=float(input("Introduce un numero decimal: "))
-num_truncado=math.trunc(num)
-print("El numero truncado es: ", num_truncado)
+class JSONFileHandler:
+    def write_json(self, file_path):
+        try:
+            d={"DNI": 24525402, "Fecha de nacimiento": "16/03/2003"};
+            with open(file_path, 'w') as f:
+                json.dump(d, f)
+        except Exception as e:
+            print("Error escribiendo JSON: {e}");
+
+    def read_json(self, file_path):
+        try:
+            with open(file_path, 'r') as f:
+                return json.load(f)
+        except Exception as e:
+            print("Error escribiendo JSON: {e}");
+
+
+json_handler=JSONFileHandler()
+json_handler.write_json('data.json')
+data=json_handler.read_json('data.json')
+print(data)
