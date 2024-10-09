4610b71f7711a18822870bca5fb09cacaf5e51f7
andreesramos <andresrn16@gmail.com>
2024-10-09 17:35:28 +0200

    actividad 2

--- Tema2/actividades/actividad2.py
+++ Tema2/actividades/actividad2.py
@@ -9,7 +9,22 @@ conexion = pymysql.connect(
 )
     
 cursor=conexion.cursor()
-cursor.execute("INSERT INTO ObrasArte (titulo, artista, fechaCreacion, tecnica, museo) VALUES (%s, %s, %s, %s, %s), ")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS ObrasArte(
+        titulo varchar(20) primary key,
+        artista varchar(30),
+        fechaCreacion varchar(10),
+        tecnica varchar(15),
+        museo varchar(15)
+    )
+""")
+
+cursor.execute(
+    "INSERT INTO ObrasArte (titulo, artista, fechaCreacion, tecnica, museo) VALUES (%s, %s, %s, %s, %s)",
+    ("La ultima cena", "Leonardo da Vinci", "1495", "Oleo temple", "Santa Maria")
+)
+conexion.commit()
+
 cursor.execute("SELECT * FROM ObrasArte")
 for fila in cursor.fetchall():
     print(fila)
