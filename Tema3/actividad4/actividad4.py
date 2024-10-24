4d9078210940099a5ea55d4fdb7c609ec892e176
andreesramos <andresrn16@gmail.com>
2024-10-24 10:05:08 +0200

    a

--- Tema3/actividad4/actividad4.py
+++ Tema3/actividad4/actividad4.py
@@ -1,4 +1,5 @@
 from peewee import MySQLDatabase, CharField, IntegerField, Model
+from peewee import IntegrityError
 
 #Conexión a la base de datos
 db=MySQLDatabase(
@@ -45,8 +46,14 @@ for o in obras:
 
 #Mostramos las obras al eliminar las que contienen los siguientes nombres
 print("\nTras eliminar: ")
-obra=ObrasArte.get((ObrasArte.titulo == 'las Meninas') & (ObrasArte.artista == 'Velazquez'))
-obra.delete_instance()
+try:
+    #Iniciamos transaccion
+    with db.atomic():
+        obra=ObrasArte.get((ObrasArte.titulo == 'las Meninas') & (ObrasArte.artista == 'Velazquez'))
+        obra.delete_instance()
+except IntegrityError as e:
+    print(f"Error al eliminar obras de arte: {e}")
+
 obras=ObrasArte.select()
 for o in obras:
     print(f"Titulo: {o.titulo}, Artista: {o.artista}, Fecha de creacion: {o.fechaCreacion}, Tecnica: {o.tecnica}, Museo: {o.museo}")
@@ -60,7 +67,13 @@ for o in obras:
 
 #Mostramos las obras al eliminar las creadas con la técnica de Óleo
 print("\nTras eliminar: ")
-ObrasArte.delete().where(ObrasArte.tecnica == 'Oleo').execute()
+try:
+    #Iniciamos transaccion
+    with db.atomic():
+        ObrasArte.delete().where(ObrasArte.tecnica == 'Oleo').execute()
+except IntegrityError as e:
+    print(f"Error al eliminar obras de arte: {e}")
+    
 obras=ObrasArte.select()
 for o in obras:
     print(f"Titulo: {o.titulo}, Artista: {o.artista}, Fecha de creacion: {o.fechaCreacion}, Tecnica: {o.tecnica}, Museo: {o.museo}")
