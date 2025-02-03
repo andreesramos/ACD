9f5d6bf4db63810dd654b21609bb97213ff93dfd
andreesramos <andresrn16@gmail.com>
2025-02-03 19:53:37 +0100

    a

--- Tema8/actividad1/actividad1.py
+++ Tema8/actividad1/actividad1.py
@@ -9,11 +9,11 @@ df=df.dropna()
 
 precios_marca = df.groupby('brand')['price_euros'].mean()
 
-#Crear gráfico de líneas
+#Gráfico de líneas
 precios_marca.plot(title='Precios según la marca', figsize=(10, 6))
 plt.show()
 
-#Crear gráfico de dispersión
+#Gráfico de dispersión
 plt.figure(figsize=(10, 6))
 plt.scatter(df['brand'], df['price_euros'], alpha=0.6, c='blue', edgecolors='k')
 plt.title("Relación entre marca y precio", fontsize=14)
@@ -24,7 +24,7 @@ plt.grid(True, linestyle='--', alpha=0.7)
 plt.tight_layout()
 plt.show()
 
-#Crear gráfico de barras
+#Gráfico de barras
 talla_especifica = '7'
 df_talla = df[df['size'] == talla_especifica]
 df_talla = df_talla.groupby('brand')['price_euros'].mean().reset_index()
@@ -36,4 +36,25 @@ df_talla.plot(
  title=f'Precio por marca para la talla {talla_especifica}',
  figsize=(10, 8)
 )
+plt.show()
+
+# Histograma
+df['price_euros'].plot(
+   kind='hist',
+   bins=20,
+   title='Distribución de los precios por marca',
+   figsize=(10, 6)
+)
+plt.show()
+
+# Diagrama de caja
+plt.boxplot(
+   df['price_euros'].dropna(),
+   patch_artist=True,
+   boxprops=dict(facecolor="lightblue", color="blue"),
+   medianprops=dict(color="red"),
+   flierprops=dict(marker="o", color="darkorange", markersize=5)
+)
+plt.title("Diagrama de caja: Precios")
+plt.xlabel("Cantidad de precios")
 plt.show()
